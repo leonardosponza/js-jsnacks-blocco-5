0f08ef217fc8c4193b3bf68a07937dd2c877d2c1
leonardo <sponzal4@gmail.com>
2021-06-15 14:48:32 +0200

    snack1

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//  Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
+
+// var biciclette = [
+//     {   nome : "tundem",
+//         peso : 39,
+//     },
+
+//     {   nome : "harry",
+//         peso : 30,
+//     },
+
+//     {
+//         nome: 'stigatsby',
+//         peso: 50,
+//     }
+
+// ];
+
+// var pesomin = biciclette[0].peso;
+
+// for(var i=1; i < biciclette.length; i++){
+//     if(biciclette[i].peso < pesomin.peso){
+//         pesomin = biciclette[i];        
+//     }
+// }
+
+// console.log(pesomin);
