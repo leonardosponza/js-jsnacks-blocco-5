8c2470e6564c75b3bfd76a687a82130ac1e86eec
leonardo <sponzal4@gmail.com>
2021-06-15 14:49:27 +0200

    snack2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//  Creare un array di oggetti:
+//   Creare un array di oggetti:
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
 
 // var biciclette = [
@@ -26,3 +26,74 @@
 // }
 
 // console.log(pesomin);
+
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine.
+
+  var zucchine = [
+     {   nome : 'tundem',
+          peso : 39,
+          lunghezza:10,
+      },
+
+     {   nome : 'harry',
+          peso : 30,
+          lunghezza:16,
+      },
+
+      {
+          nome: 'lola',
+          peso: 50,
+          lunghezza:20,
+      },
+
+      {
+         nome: 'gianni',
+         peso: 10,
+         lunghezza:18,
+     },
+
+     {
+         nome: 'stigatsby',
+         peso: 8,
+         lunghezza:7,
+     },
+
+     {
+         nome: 'harry',
+         peso: 20,
+         lunghezza:10,
+     },
+
+     {
+         nome: 'manu',
+         peso: 23,
+         lunghezza:9,
+     },
+
+     {
+         nome: 'toni',
+         peso: 4,
+         lunghezza:3,
+     },
+
+     {
+         nome: 'verdana',
+         peso: 7,
+         lunghezza:11,
+     },
+
+     {
+         nome: 'molise',
+         peso: 21,
+         lunghezza:15,
+     }
+
+  ];
+
+  var pesotot = 0;
+
+  for(var i = 0; i<zucchine.length; i++){
+      console.log(zucchine[i].peso)
+      pesotot += zucchine[i].peso
+  }
+  console.log(pesotot);
